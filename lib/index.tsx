951db9c6c5c558f8d0bf5367cd836ceb223f1cd6
Frank Fang <FrankFang1990@gmail.com>
2019-03-08 21:50:14 +0800

    webpack.config 分成了两份，index.d.ts 移到 dist 目录里了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Button from './button';
 
-ReactDOM.render(<Button></Button>, document.body);
+const a = 1
+
+export {Button, a};
