c101cf6a508d205164dd7721a677a1d3256cde18
Frank Fang <FrankFang1990@gmail.com>
2019-03-08 21:00:36 +0800

    安装了 React 和 ReactDOM！

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,5 @@
-const div = document.createElement('div')
-div.innerText='hi'
-document.body.appendChild(div)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(React)
+console.log(ReactDOM)
