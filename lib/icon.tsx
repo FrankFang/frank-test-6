8f1aa56f95410fb62bb67dca9ba649789acf1de4
Frank Fang <FrankFang1990@gmail.com>
2019-03-15 21:09:57 +0800

    添加 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,11 +4,12 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: () => void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="fui-icon">
+    <svg className="fui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}/>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Icon from './icon';
 
+const fn = () => {
+  console.log('fn');
+};
+
 ReactDOM.render(<div>
-  <Icon name="wechat"/>
-  <Icon name="alipay"/>
-  <Icon name="qq"/>
+  <Icon name="qq" onClick={fn}/>
 </div>, document.querySelector('#root'));
