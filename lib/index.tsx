4aa27ea5cfe67892dd37672fcb4eec9fe3c8158b
Frank Fang <FrankFang1990@gmail.com>
2019-03-22 22:51:09 +0800

    update

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,3 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
+export {default as Icon} from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
 
-ReactDOM.render(<div>
-  <Icon name="qq"
-        onClick={fn}
-        onMouseEnter={() => console.log('enter')}
-        onMouseLeave={() => console.log('leave')}
-        onTouchStart={() => console.log('touch')}
-  />
-</div>, document.querySelector('#root'));
